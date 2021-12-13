fdf63ec82facd98494b494a3f7145338367b625c
AnupJonchhe <anup@broadinstitute.org>
2021-12-13 12:30:38 -0500

    Added docker container for AWS

---
+++ docker/build_docker.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build -t prismcmap/sushi:latest -t prismcmap/sushi:v0.0.2 --rm=true .
---
+++ docker/docker_push_tags.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker push prismcmap/sushi:v0.0.2
+docker push prismcmap/sushi:latest
