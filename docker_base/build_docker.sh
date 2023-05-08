b20044659f7eb9464840c98c268fbeaee09fa457
Maha Naim <naim@broadinstitute.org>
2023-05-08 10:32:57 -0400

    Updates to docker containers

--- docker_base/build_docker.sh
+++ docker_base/build_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #change the version number for each new build
-docker build -t prismcmap/base-clue-pseq:latest -t prismcmap/base-clue-pseq:v0.0.1 --rm=true .
+docker build --platform linux/amd64 -t prismcmap/base-clue-pseq:latest -t prismcmap/base-clue-pseq:v0.0.1 --rm=true .
