3ffc14b6697323481a422cde57986557bb4709c0
YuhJong Liu <yuhjong24@gmail.com>
2023-10-16 13:26:42 -0400

    Updated group by for additional columns

--- R/collapse_counts.R
+++ R/collapse_counts.R
@@ -8,9 +8,9 @@
 collapse_counts = function(l2fc) {
   collapsed_counts = l2fc %>% 
     dplyr::filter(control_pass_QC) %>% 
-    dplyr::group_by_at(setdiff(names(.), c('bio_rep', 'mean_normalized_n', 'num_tech_reps', 
-                                           'control_median_normalized_n', 'control_mad_sqrtN', 'num_ctrl_bio_reps', 'control_pass_QC',
-                                           'l2fc'))) %>% 
+    dplyr::group_by_at(setdiff(names(.), c('bio_rep', 'mean_n','mean_normalized_n', 'num_tech_reps', 'control_median_n',
+                                           'control_median_normalized_n', 'control_mad_sqrtN', 'num_ctrl_bio_reps', 
+                                           'control_pass_QC','l2fc', 'flag'))) %>% 
     dplyr::summarise(trt_median_normalized_n= median(mean_normalized_n),
                      median_l2fc= median(l2fc), num_bio_reps= n(),
                      trt_mad_sqrtN= mad(log2(mean_normalized_n)) / sqrt(dplyr::n())) %>% 
