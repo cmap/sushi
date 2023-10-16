67f6defe4c957556d5e3bc3c980202ed58fb47e8
YuhJong Liu <yuhjong24@gmail.com>
2023-10-16 13:26:13 -0400

    Added collapsed raw counts

--- R/compute_l2fc.R
+++ R/compute_l2fc.R
@@ -18,7 +18,7 @@ compute_l2fc = function(normalized_counts,
                         control_type = "negcon",
                         sig_cols=c('cell_set','treatment','dose','dose_unit','day'),
                         ctrl_cols= c('cell_set', 'day'), # will probably be a subset of sig_cols
-                        count_col_name="normalized_n") {
+                        count_col_name="normalized_n", count_threshold= 40) {
   
   if(!all(ctrl_cols %in% sig_cols)) {
     print("Control columns are not a subset of sig columns.") # new
@@ -31,9 +31,10 @@ compute_l2fc = function(normalized_counts,
   collapsed_tech_rep= normalized_counts %>%
     dplyr::filter(!(trt_type %in% c("empty", "", "CB_only")) & !is.na(trt_type), !is.na(CCLE_name)) %>%
     dplyr::group_by_at(setdiff(names(.), c('pcr_plate','pcr_well', 'Name', 'log2_dose', 'cb_intercept',
-                                           'profile_id', 'tech_rep', 'n', 'log2_n', 'normalized_n', 'log2_normalized_n',
+                                           'profile_id', 'tech_rep', 'n', 'log2_np1', 'normalized_n', 'log2_normalized_n',
                                            'flag', count_col_name))) %>% 
-    dplyr::summarise(mean_normalized_n = mean(!! rlang::sym(count_col_name)), 
+    dplyr::summarise(mean_n= mean(n),
+                     mean_normalized_n = mean(!!rlang::sym(count_col_name)), 
                      num_tech_reps= n()) %>% 
     dplyr::ungroup()
   
@@ -41,11 +42,12 @@ compute_l2fc = function(normalized_counts,
   controls= collapsed_tech_rep %>% 
     dplyr::filter(trt_type==control_type) %>% 
     dplyr::group_by_at(c('project_code', 'CCLE_name', 'DepMap_ID', 'prism_cell_set', ctrl_cols)) %>% 
-    dplyr::summarise(control_median_normalized_n = median(mean_normalized_n),
+    dplyr::summarise(control_median_n= median(mean_n),
+                     control_median_normalized_n = median(mean_normalized_n),
                      control_mad_sqrtN = mad(log2(mean_normalized_n))/sqrt(dplyr::n()),
                      num_ctrl_bio_reps = n()) %>% 
     dplyr::ungroup() %>% 
-    dplyr::mutate(control_pass_QC = ifelse(control_mad_sqrtN > 0.5/log10(2), F, T)) #%>% # New: adjusted cut off to log2
+    dplyr::mutate(control_pass_QC = ifelse(control_mad_sqrtN > 0.5/log10(2), F, T)) # New: adjusted cut off to log2
   
   if(nrow(controls)==0) {
     print("No samples found for indicated control type.")
@@ -54,7 +56,8 @@ compute_l2fc = function(normalized_counts,
   
   l2fc= collapsed_tech_rep %>% dplyr::filter(!trt_type %in% c(control_type, 'day_0')) %>% 
     merge(controls, by= c('project_code',"CCLE_name", "DepMap_ID", "prism_cell_set", ctrl_cols), all.x=T, all.y=T) %>%
-    dplyr::mutate(l2fc= log2(mean_normalized_n/control_median_normalized_n)) %>%
+    dplyr::mutate(l2fc= log2(mean_normalized_n/control_median_normalized_n),
+                  flag= ifelse(control_median_n < count_threshold, paste0('negcon<', count_threshold), NA)) %>%
     dplyr::relocate(project_code, CCLE_name, DepMap_ID, prism_cell_set, trt_type, control_barcodes, sig_id, bio_rep)
   
   return(l2fc)
