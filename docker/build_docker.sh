0e298473c5bcb8a2b4a578a27eb1c6717758cc54
Anup Jonchhe <anup@broadinstitute.org>
2024-01-22 09:48:12 -0500

    updates to dockers

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-docker build -t prismcmap/sushi:latest -t prismcmap/sushi:v1.0.1 --rm=true .
+
+cd ../
+
+docker build -t prismcmap/sushi:develop --rm=true -f docker/Dockerfile . --no-cache
