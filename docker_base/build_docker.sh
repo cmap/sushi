cbb5ae90f181604d70d373c2712b07b23e58e574
jdavis3141 <jdavis@broadinstitute.org>
2024-06-14 09:58:49 -0400

    Updating with proper tags and dockerfile location

--- docker_base/build_docker.sh
+++ docker_base/build_docker.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-#change the version number for each new build
-podman build --platform linux/amd64 -t prismcmap/base-clue-pseq:develop --rm=true .
+# Ensure script stops on first error
+set -e
+
+# Change the version number for each new build
+# Specify the directory containing the Dockerfile
+BUILD_CONTEXT_DIR="./docker_base"
+
+# Run the podman build command with the specified context directory
+podman build --platform linux/amd64 -t podman_base_image:develop --rm=true -f ${BUILD_CONTEXT_DIR}/Dockerfile ${BUILD_CONTEXT_DIR}
