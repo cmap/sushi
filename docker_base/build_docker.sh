3c96bf8bb8082bb1929c7a08dd2c1717aaf2b008
jdavis3141 <jdavis@broadinstitute.org>
2024-06-27 10:20:07 -0400

    Update build_docker.sh

--- docker_base/build_docker.sh
+++ docker_base/build_docker.sh
@@ -7,4 +7,5 @@ set -e
 BUILD_CONTEXT_DIR="./docker_base"
 
 # Run the podman build command with the specified context directory
-podman build -t sushi-podman -f ${BUILD_CONTEXT_DIR}/Dockerfile ${BUILD_CONTEXT_DIR}
+cd ${BUILD_CONTEXT_DIR}
+podman build -t sushi-podman .
