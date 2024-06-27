c0203a9f3fcf631b4f65e24b81923d86d20ab25b
jdavis3141 <jdavis@broadinstitute.org>
2024-06-27 10:22:15 -0400

    Update build_docker.sh

--- docker_base/build_docker.sh
+++ docker_base/build_docker.sh
@@ -3,9 +3,9 @@
 # Ensure script stops on first error
 set -e
 
+# Change the version number for each new build
 # Specify the directory containing the Dockerfile
 BUILD_CONTEXT_DIR="./docker_base"
 
 # Run the podman build command with the specified context directory
-cd ${BUILD_CONTEXT_DIR}
-podman build -t sushi-podman .
+podman build -t podman_base_image:develop --rm=true -f ${BUILD_CONTEXT_DIR}/Dockerfile ${BUILD_CONTEXT_DIR}
