caf700dedb68e684f8e26356d5eec345b8869087
YuhJong Liu <yuhjong24@gmail.com>
2024-07-01 11:31:19 -0400

    Added sample meta as a parameter

--- scripts/collapse_replicates.R
+++ scripts/collapse_replicates.R
@@ -6,10 +6,12 @@ parser <- ArgumentParser()
 # specify our desired options 
 parser$add_argument("-v", "--verbose", action="store_true", default=TRUE,
                     help="Print extra output [default]")
-parser$add_argument("-q", "--quietly", action="store_false", 
-                    dest="verbose", help="Print little output")
+parser$add_argument("-q", "--quietly", action="store_false", dest="verbose", 
+                    help="Print little output")
 parser$add_argument("-c", "--lfc", default="l2fc.csv",
                     help="path to file containing l2fc values")
+parser$add_argument("-s", "--sample_meta", default="sample_meta.csv", 
+                    help = "Sample metadata")
 parser$add_argument("--sig_cols", default="cell_set,treatment,dose,dose_unit,day", 
                     help = "columns used to identify a unique condition")
 parser$add_argument("-o", "--out", default=getwd(), help = "Output path. Default is working directory")
@@ -18,10 +20,11 @@ parser$add_argument("-o", "--out", default=getwd(), help = "Output path. Default
 args <- parser$parse_args()
 
 lfc_values= data.table::fread(args$lfc, header=T, sep=',')
+sample_meta= data.table::fread(args$sample_meta, header=T, sep=',')
 
 print("Collapsing biological replicates ...")
 sig_cols = unlist(strsplit(args$sig_cols, ","))
-collapsed_counts= collapse_counts(lfc_values, sig_cols)
+collapsed_counts= collapse_counts(lfc_values, sample_meta, sig_cols)
 
 # Write out file
 collapsed_count_out_file= paste(args$out, "collapsed_values.csv", sep='/')
