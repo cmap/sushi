0174ccedef8e2bd9a65c9ce70ff706da11d0e364
jdavis3141 <jdavis@broadinstitute.org>
2024-07-10 13:51:04 -0400

    simplifying biocmanager install

--- docker_base/install_packages.R
+++ docker_base/install_packages.R
@@ -1,8 +1,5 @@
 install.packages("argparse")
 install.packages("magrittr")
 install.packages("tidyverse")
-
-if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-
+install.packages("BiocManager")
 BiocManager::install("cmapR")
