a346412576bea65dcdf19ad15941d446b62e0aca
jdavis3141 <jdavis@broadinstitute.org>
2024-07-10 12:05:48 -0400

    Update install_packages.R
    add tidyverse dependencies

--- docker_base/install_packages.R
+++ docker_base/install_packages.R
@@ -4,26 +4,75 @@ options(repos = c(
 
 options(repos=structure(c(CRAN="http://cran.r-project.org")))
 
-install.packages("tidyverse")
-install.packages("hdf5r")
-install.packages("reshape2")
-install.packages("readr")
-install.packages("magrittr")
-install.packages("dr4pl")
-install.packages("data.table")
-install.packages("scam")
-install.packages("argparse")
-install.packages("splitstackshape")
-install.packages("BiocManager")
+install_and_log <- function(pkg) {
+  tryCatch(
+    {
+      install.packages(pkg)
+      message(sprintf("Successfully installed %s", pkg))
+    },
+    error = function(e) {
+      message(sprintf("Failed to install %s: %s", pkg, e$message))
+    }
+  )
+}
+
+bioc_install_and_log <- function(pkg) {
+  tryCatch(
+    {
+      BiocManager::install(pkg, ask = FALSE)
+      message(sprintf("Successfully installed %s", pkg))
+    },
+    error = function(e) {
+      message(sprintf("Failed to install %s: %s", pkg, e$message))
+    }
+  )
+}
+
+# Install dependencies for tidyverse
+install_and_log("googledrive")
+install_and_log("googlesheets4")
+install_and_log("haven")
+install_and_log("httr")
+install_and_log("ragg")
+install_and_log("rvest")
+install_and_log("xml2")
+
+# Install tidyverse
+install_and_log("tidyverse")
+
+install_and_log("hdf5r")
+install_and_log("reshape2")
+install_and_log("readr")
+install_and_log("magrittr")
+install_and_log("dr4pl")
+install_and_log("data.table")
+install_and_log("scam")
+install_and_log("argparse")
+install_and_log("splitstackshape")
+install_and_log("BiocManager")
 BiocManager::install()
-BiocManager::install("sva")
-BiocManager::install("limma", ask = FALSE)
-BiocManager::install('apeglm')
-BiocManager::install('ShortRead')
-install.packages("PRROC")
-install.packages("gmodels")
-install.packages("R.utils")
-#install.packages("devtools")
-#devtools::install_github("broadinstitute/cdsr_models", dependencies=TRUE, force=TRUE)
-#devtools::install_github("broadinstitute/cdsr_biomarker")
-#devtools::install_github('https://github.com/cmap/sushi')
+
+bioc_install_and_log("sva")
+bioc_install_and_log("limma")
+bioc_install_and_log("apeglm")
+bioc_install_and_log("ShortRead")
+
+install_and_log("PRROC")
+install_and_log("gmodels")
+install_and_log("R.utils")
+
+# Uncomment these lines if you need the devtools packages
+#install_and_log("devtools")
+#tryCatch(
+#  devtools::install_github("broadinstitute/cdsr_models", dependencies=TRUE, force=TRUE),
+#  error = function(e) message(sprintf("Failed to install cdsr_models: %s", e$message))
+#)
+#tryCatch(
+#  devtools::install_github("broadinstitute/cdsr_biomarker"),
+#  error = function(e) message(sprintf("Failed to install cdsr_biomarker: %s", e$message))
+#)
+#tryCatch(
+#  devtools::install_github('https://github.com/cmap/sushi'),
+#  error = function(e) message(sprintf("Failed to install sushi: %s", e$message))
+#)
+
