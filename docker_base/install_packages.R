a4b30ffa608711f8a6f52c99f4adb5758b850922
jdavis3141 <jdavis@broadinstitute.org>
2024-07-10 13:34:28 -0400

    added cmapR install

--- docker_base/install_packages.R
+++ docker_base/install_packages.R
@@ -1,3 +1,8 @@
 install.packages("argparse")
 install.packages("magrittr")
 install.packages("tidyverse")
+
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("cmapR")
