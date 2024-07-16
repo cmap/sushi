1b4965d10c09fa45587a3b5b0f0bd8ae472fc1fe
jdavis3141 <jdavis@broadinstitute.org>
2024-07-16 15:53:56 -0400

    Updating make_config_file.groovy to allow existing config.json

--- scripts/make_config_file.groovy
+++ scripts/make_config_file.groovy
@@ -1,3 +1,5 @@
+import groovy.json.JsonSlurper
+
 pipeline {
     agent any
 
@@ -38,6 +40,10 @@ pipeline {
         string(name: 'API_URL', defaultValue: 'https://api.clue.io/api/', description: 'API URL')
     }
 
+    environment {
+        CONFIG_FILE_PATH = "${params.BUILD_DIR}/config.json"
+    }
+
     stages {
         stage('Checkout') {
             steps {
@@ -63,12 +69,61 @@ pipeline {
                 }
             }
         }
+
         stage('Generate JSON Config') {
             steps {
                 script {
-                    def config = params.keySet().findAll { it != 'TRIGGER_BUILD' && it != 'COMMIT_HASH' }.collectEntries { [(it): params[it]] }
-                    config.API_KEY = sh(script: 'cat /local/jenkins/.clue_api_key', returnStdout: true).trim()
-                    writeFile file: "${params.BUILD_DIR}/config.json", text: groovy.json.JsonOutput.prettyPrint(groovy.json.JsonOutput.toJson(config))
+                    def config = [:]
+                    if (fileExists(env.CONFIG_FILE_PATH)) {
+                        def configText = readFile(file: env.CONFIG_FILE_PATH)
+                        config = new JsonSlurper().parseText(configText)
+                    }
+
+                    def paramsMap = [
+                        GIT_BRANCH: params.GIT_BRANCH,
+                        COMMIT_HASH: params.COMMIT_HASH,
+                        BUILD_DIR: params.BUILD_DIR,
+                        BUILD_NAME: params.BUILD_NAME,
+                        SEQ_TYPE: params.SEQ_TYPE,
+                        CTL_TYPES: params.CTL_TYPES,
+                        DAYS: params.DAYS,
+                        CELL_SET_META: params.CELL_SET_META,
+                        ID_COLS: params.ID_COLS,
+                        SAMPLE_COLS: params.SAMPLE_COLS,
+                        SIG_COLS: params.SIG_COLS,
+                        SEQUENCING_INDEX_COLS: params.SEQUENCING_INDEX_COLS,
+                        CONTROL_BARCODE_META: params.CONTROL_BARCODE_META,
+                        CONTROL_COLS: params.CONTROL_COLS,
+                        REMOVE_DATA: params.REMOVE_DATA,
+                        COUNT_COL_NAME: params.COUNT_COL_NAME,
+                        RUN_NORM: params.RUN_NORM,
+                        PULL_POOL_ID: params.PULL_POOL_ID,
+                        CONVERT_SUSHI: params.CONVERT_SUSHI,
+                        RUN_EPS_QC: params.RUN_EPS_QC,
+                        SAMPLE_META: params.SAMPLE_META,
+                        COUNT_THRESHOLD: params.COUNT_THRESHOLD,
+                        PSEUDOCOUNT: params.PSEUDOCOUNT,
+                        CELL_LINE_META: params.CELL_LINE_META,
+                        RAW_COUNTS: params.RAW_COUNTS,
+                        FILTERED_COUNTS: params.FILTERED_COUNTS,
+                        LFC: params.LFC,
+                        ANNOTATED_COUNTS: params.ANNOTATED_COUNTS,
+                        NORMALIZED_COUNTS: params.NORMALIZED_COUNTS,
+                        COLLAPSED_VALUES: params.COLLAPSED_VALUES,
+                        API_URL: params.API_URL
+                    ]
+
+                    paramsMap.each { key, value ->
+                        if (value) {
+                            config[key] = value
+                        }
+                    }
+
+                    if (!config.containsKey('API_KEY')) {
+                        config.API_KEY = sh(script: 'cat /local/jenkins/.clue_api_key', returnStdout: true).trim()
+                    }
+
+                    writeFile file: env.CONFIG_FILE_PATH, text: groovy.json.JsonOutput.prettyPrint(groovy.json.JsonOutput.toJson(config))
                     echo "Generated config.json: ${config}"
                 }
             }
@@ -78,7 +133,7 @@ pipeline {
             steps {
                 script {
                     def commitHash = sh(script: 'git rev-parse HEAD', returnStdout: true).trim()
-                    def configFilePath = "${params.BUILD_DIR}/config.json"
+                    def configFilePath = env.CONFIG_FILE_PATH
                     sh """
                         jq --arg commit "$commitHash" '. + {COMMIT: \$commit}' $configFilePath > ${configFilePath}.tmp && mv ${configFilePath}.tmp $configFilePath
                     """
@@ -92,12 +147,13 @@ pipeline {
         success {
             script {
                 if (params.TRIGGER_BUILD) {
-                    def nextJobParams = params.keySet().collect {
-                        def param = params[it]
-                        if (param instanceof Boolean) {
-                            booleanParam(name: it, value: param)
+                    def configText = readFile(file: env.CONFIG_FILE_PATH)
+                    def config = new JsonSlurper().parseText(configText)
+                    def nextJobParams = config.collect { key, value ->
+                        if (value == 'true' || value == 'false') {
+                            booleanParam(name: key, value: value.toBoolean())
                         } else {
-                            string(name: it, value: param.toString())
+                            string(name: key, value: value.toString())
                         }
                     }
                     build job: 'create_celldb_metadata_podman', wait: false, parameters: nextJobParams
