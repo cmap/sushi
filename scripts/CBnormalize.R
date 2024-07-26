99d36752635983038866ac5d503cccdbc5e447b2
YuhJong Liu <yuhjong24@gmail.com>
2024-07-26 11:11:56 -0400

    Update CBnormalize.R

--- scripts/CBnormalize.R
+++ scripts/CBnormalize.R
@@ -36,5 +36,5 @@ normalized_counts = normalize(X= filtered_counts, id_cols= input_id_cols,
 
 # Write out file ----
 normcounts_outpath = paste(args$out, "normalized_counts.csv", sep='/')
-print(paste("Writing normalized count file to ", normcounts_outpath))
+print(paste0("Writing normalized count file to ", normcounts_outpath))
 write.csv(normalized_counts, normcounts_outpath, row.names= FALSE, quote= FALSE)
