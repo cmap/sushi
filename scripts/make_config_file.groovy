282ebb2d1028d9e21480e90b2b5c5549199de71c
jdavis3141 <jdavis@broadinstitute.org>
2024-07-31 18:56:50 -0400

    adding filteredcounts_qc

--- scripts/make_config_file.groovy
+++ scripts/make_config_file.groovy
@@ -11,6 +11,7 @@ pipeline {
         booleanParam(name: 'CREATE_CELLDB_METADATA', defaultValue: true, description: 'Check this to trigger the create_celldb_metadata job.')
         booleanParam(name: 'COLLATE_FASTQ_READS', defaultValue: true, description: 'Check this to trigger the collate_fastq_reads job.')
         booleanParam(name: 'FILTER_COUNTS', defaultValue: true, description: 'Check this to trigger the filter_counts job.')
+        booleanParam(name: 'FILTER_COUNTS_QC', defaultValue: true, description: 'Check this to trigger the filteredCounts_QC job.')
         booleanParam(name: 'CBNORMALIZE', defaultValue: true, description: 'Check this to trigger the CBnormalize job.')
         booleanParam(name: 'COMPUTE_LFC', defaultValue: true, description: 'Check this to trigger the compute_l2fc job.')
         booleanParam(name: 'COLLAPSE', defaultValue: true, description: 'Check this to trigger the collapse job.')
@@ -161,6 +162,9 @@ pipeline {
                     }
                     if (params.FILTER_COUNTS) {
                         scriptsToRun.add('filter_counts.sh')
+                    }
+                    if (params.FILTER_COUNTS_QC) {
+                        scriptsToRun.add('filteredCounts_QC.sh')
                     }
                     if (params.CBNORMALIZE) {
                         scriptsToRun.add('CBnormalize.sh')
