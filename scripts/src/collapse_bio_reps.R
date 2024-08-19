ee8fb8831ae42246ba6806f067befe808951fefb
jdavis3141 <jdavis@broadinstitute.org>
2024-08-19 16:47:52 -0400

    Debugging collapsed counts

--- scripts/src/collapse_bio_reps.R
+++ scripts/src/collapse_bio_reps.R
@@ -45,7 +45,6 @@ validate_num_bio_reps= function(max_bio_rep_id, max_bio_rep_count) {
 #' @returns - collapsed_counts
 collapse_bio_reps = function(l2fc) {
   collapsed_counts = l2fc %>%
-    dplyr::filter(is.na(counts_flag)) %>%
     dplyr::group_by_at(setdiff(names(.), c('bio_rep', 'mean_n','mean_normalized_n', 'num_tech_reps', 'control_median_n',
                                            'control_median_normalized_n', 'control_mad_sqrtN', 'num_ctrl_bio_reps',
                                            'control_MAD_QC','l2fc', 'counts_flag', 'lysate_plate', 'lysate_well', 'pert_plate','sig_id'))) %>%
@@ -56,6 +55,7 @@ collapse_bio_reps = function(l2fc) {
     dplyr::mutate(trt_MAD_QC= ifelse(trt_mad_sqrtN > 0.5/log10(2), F, T)) %>% # New: adjusted cut off to log2
     dplyr::relocate(trt_median_n, trt_median_normalized_n, trt_mad_sqrtN,
                     num_bio_reps, median_l2fc, trt_MAD_QC, .after=last_col())
+  print(collapsed_counts)
 
 }
   return(collapsed_counts)
