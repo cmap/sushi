e85ca76a056b16122806689152008ffeefea1aeb
maha-naim <naim@broadinstitute.org>
2024-09-06 14:58:14 -0400

    Refactored filter counts code and added tests for validation

--- scripts/filter_counts.R
+++ scripts/filter_counts.R
@@ -9,151 +9,194 @@ suppressPackageStartupMessages(library(tidyr)) #pivot_wider
 suppressPackageStartupMessages(library(sets))
 suppressPackageStartupMessages(library(tidyverse)) # load last - after dplyr
 source("./src/filter_raw_reads.R")
+filtered_counts_required_cols <- c("CCLE_name", "cell_set", "DepMap_ID")
+assay_pool_required_cols <- c("ccle_name", "davepool_id", "depmap_id", "pool_id")
+cell_line_required_cols <- c("CCLE_name", "DepMap_ID", "LUA", "Sequence")
 
-# Arguement parser ----
-parser <- ArgumentParser()
-# specify desired options
-parser$add_argument("-v", "--verbose", action="store_true", default=TRUE,
-                    help="Print extra output [default]")
-parser$add_argument("-q", "--quietly", action="store_false",
-                    dest="verbose", help="Print little output")
-parser$add_argument("--wkdir", default=getwd(), help="Working directory")
-parser$add_argument("-c", "--raw_counts", default="raw_counts.csv", help = "path to file containing raw counts")
-parser$add_argument("-o", "--out", default="", help = "Output path. Default is working directory")
-parser$add_argument("-s", "--sample_meta", default="sample_meta.csv", help = "Sample metadata")
-parser$add_argument("--cell_line_meta", default="cell_line_meta.csv", help = "Cell Line metadata")
-parser$add_argument("--cell_set_meta", default="cell_set_meta.csv", help = "Cell set metadata")
-parser$add_argument("--assay_pool_meta", default="assay_pool_meta.txt", help = "Assay pool metadata")
-parser$add_argument("--CB_meta", default="CB_meta.csv", help = "Control Barcode metadata")
-parser$add_argument("--sequencing_index_cols", default= "index_1,index_2", 
-                    help = "Sequencing columns in the sample meta")
-parser$add_argument("--count_threshold", default= 40, help = "Low counts threshold")
-parser$add_argument("--reverse_index2", type="logical",
-                    help = "Reverse complement of index 2 for NovaSeq and NextSeq")
-parser$add_argument("--rm_data", type="logical", help = "Remove bad experimental data")
-parser$add_argument("--pool_id", type="logical", help = "Pull pool IDs from CellDB.")
-parser$add_argument("--control_type", default="negcon", 
-                    help = "negative control wells in trt_type column in sample metadata")
+##### FUNCTIONS #####
 
-# get command line options, if help option encountered print help and exit
-args <- parser$parse_args()
-
-# set output to working directory if none is specified
-if (args$out == ""){
-  args$out = args$wkdir
-}
-#print_args(args)
-
-# Read in files and set up parameters ----
-cell_set_meta= data.table::fread(args$cell_set_meta, header= T, sep= ',', data.table= F)
-cell_line_meta= data.table::fread(args$cell_line_meta, header= T, sep= ',', data.table= F)
-CB_meta= data.table::fread(args$CB_meta, header= T, sep= ',', data.table= F)
-sample_meta= data.table::fread(args$sample_meta, header= T, sep= ',', data.table= F)
-raw_counts= data.table::fread(args$raw_counts, header= T, sep= ',', data.table= F)
-
-# Convert strings to vectors ----
-# Also check that column names are present in the sample meta.
-sequencing_index_cols= unlist(strsplit(args$sequencing_index_cols, ","))
-if (!all(sequencing_index_cols %in% colnames(sample_meta))){
-  stop(paste("All seq columns not found in sample_meta, check metadata or --sequencing_index_cols argument:",
-             args$sequencing_index_cols))
+#' validate_seq_index_cols
+#' 
+#' This function checks that a list of sequencing index columns are present in the sample_meta.
+#' 
+#' @param sequencing_index_cols A vector of strings each representing a column name
+#' @param sample_meta Sample meta as a dataframe
+validate_seq_index_cols = function(sequencing_index_cols, sample_meta){
+  sequencing_index_cols_split = unlist(strsplit(sequencing_index_cols, ","))
+  if (!all(sequencing_index_cols_split %in% colnames(sample_meta))){
+    stop(paste("All seq columns not found in sample_meta, check metadata or --sequencing_index_cols argument:",
+               sequencing_index_cols_split))
+  }
 }
 
-count_threshold = as.numeric(args$count_threshold)
+# TODO
+validate_cell_line_meta = function(cell_line_meta) {
+  validate_required_cols(cell_line_meta, cell_line_required_cols)
+  # stop(paste("Cell line meta validation not yet implemented."))
+}
 
-# make sure LUA codes in cell line meta are unique
-cell_line_meta %<>% 
-  dplyr::group_by(LUA) %>% 
-  dplyr::mutate(LUA.duplicity = n()) %>% 
-  dplyr::ungroup()
+remove_duplicate_luas = function(cell_line_meta) {
+  # make sure LUA codes in cell line meta are unique
+  cell_line_meta %<>% 
+    dplyr::group_by(LUA) %>% 
+    dplyr::mutate(LUA.duplicity = n()) %>% 
+    dplyr::ungroup()
+  
+  print(paste0("LUAs that are duplicated ", 
+               dplyr::filter(cell_line_meta, LUA.duplicity > 1)$LUA %>% 
+                 unique() %>% sort() %>% paste0(collapse = ", "))) # print LUA duplicates
+  
+  cell_line_meta %<>% 
+    dplyr::filter(!duplicated(cell_line_meta$LUA, fromLast = TRUE)) %>%
+    dplyr::select(-LUA.duplicity)
+  
+  return(cell_line_meta)
+}
 
-print(paste0("LUAs that are duplicated ", 
-             dplyr::filter(cell_line_meta, LUA.duplicity > 1)$LUA %>% 
-               unique() %>% sort() %>% paste0(collapse = ", "))) # print LUA duplicates
-
-cell_line_meta %<>% 
-  dplyr::filter(!duplicated(cell_line_meta$LUA, fromLast = TRUE)) %>%
-  dplyr::select(-LUA.duplicity)
-
-# Remove flowcell_name and lane columns from sample_meta because
-# there is a profile_id duplicate when there are more than 1 seq runs
-#sample_meta %<>% select(-flowcell_name, -flowcell_lane) %>%
- # distinct() # This needs to be removed for sequencing_index_cols to work! - YL
-
-# Run filter_raw_reads -----
-print("creating filtered count file")
-filtered_counts = filter_raw_reads(raw_counts,
-                                   sample_meta,
-                                   cell_line_meta,
-                                   cell_set_meta,
-                                   CB_meta,
-                                   sequencing_index_cols= sequencing_index_cols,
-                                   count_threshold= as.numeric(args$count_threshold),
-                                   reverse_index2= args$reverse_index2)
-
-# Pulling pool_id when db_flag and pool_id flags are passed
-if (args$pool_id) {
-  assay_pool_meta = read.delim(args$assay_pool_meta)
-  unique_cell_sets <- unique(sample_meta$cell_set[sample_meta$cell_set != ""])
-  assay_pool_meta <- assay_pool_meta[assay_pool_meta$davepool_id %in% unique_cell_sets,] %>% 
+merge_pool_info = function(df, assay_pool_meta, unique_cell_sets){
+  assay_pool_subset <- assay_pool_meta[assay_pool_meta$davepool_id %in% unique_cell_sets,] %>% 
     select(pool_id, ccle_name, davepool_id, depmap_id)
+  result = df %>% merge(assay_pool_subset, by.x=filtered_counts_required_cols, 
+                         by.y=assay_pool_required_cols, all.x=T)
+  return(result)
   
-  filtered_counts$filtered_counts = filtered_counts$filtered_counts %>% 
-    merge(assay_pool_meta, by.x=c("CCLE_name", "cell_set", "DepMap_ID"), 
-          by.y=c("ccle_name", "davepool_id", "depmap_id"), all.x=T) 
-  
-  filtered_counts$annotated_counts = filtered_counts$annotated_counts %>% 
-    merge(assay_pool_meta, by.x=c("CCLE_name", "cell_set", "DepMap_ID"), 
-          by.y=c("ccle_name", "davepool_id", "depmap_id"), all.x=T)
 }
 
-# Validation: Basic file size check ----
-if(sum(filtered_counts$filtered_counts$n) == 0) {
-  stop('All entries in filtered counts are missing!')
+validate_required_cols = function(df, required_columns){
+  intersect(colnames(df), required_columns)
+  if (length(intersect(colnames(df), required_columns)) != length(required_columns)){
+    stop(paste("Required columns:", required_columns, "are not present in the provided dataframe."))
+  }
 }
 
-cl_entries= filtered_counts$filtered_counts %>% dplyr::filter(!is.na(CCLE_name))
-if(sum(cl_entries$n) == 0) {
-  stop('All cell line counts are zero!')
-}
-
-# Write out module outputs ----
-qc_table = filtered_counts$qc_table
-qc_out_file = paste(args$out, 'QC_table.csv', sep='/')
-print(paste("writing QC_table to: ", qc_out_file))
-write.csv(qc_table, qc_out_file, row.names=F, quote=F)
-
-unmapped_reads= filtered_counts$unmapped_reads
-unmapped_out = paste(args$out, 'unmapped_reads.csv', sep='/')
-print(paste("writing unmapped reads to: ", unmapped_out))
-write.csv(unmapped_reads, unmapped_out, row.names=F)
-
-annotated_counts = filtered_counts$annotated_counts
-annot_out_file = paste(args$out, 'annotated_counts.csv', sep='/')
-print(paste("writing annotated counts to: ", annot_out_file))
-write.csv(annotated_counts, annot_out_file, row.names=F)
-
-filtered_counts = filtered_counts$filtered_counts
-
-print(paste("rm_data:", args$rm_data))
-# Remove data if needed
-if(args$rm_data == TRUE){
+remove_data_from_filter_counts = function(filtered_counts, outpath) {
+  # Remove data if needed
   print('rm_data is TRUE, removing supplied data.')
-  data_to_remove <- read.csv(paste(args$out, 'data_to_remove.csv', sep='/'))
+  data_to_remove <- read.csv(paste(outpath, 'data_to_remove.csv', sep='/'))
   print('Data to remove:')
   print(head(data_to_remove))
   filt_rm <- remove_data(filtered_counts, data_to_remove)
-  
-  # keep the full filtered counts with the data that needs to be removed
-  filtered_counts_original <- filtered_counts
-  write.csv(filtered_counts_original, paste(args$out, 'filtered_counts_original.csv', sep='/'), row.names=F, quote=F)
-  # re-point to what filtered_counts should be
-  filtered_counts <- filt_rm
-  rows_removed = nrow(filtered_counts_original) - nrow(filtered_counts)
-  paste("Number of rows removed: ", rows_removed)
+  return(filt_rm)
 }
 
-filtrc_out_file = paste(args$out, 'filtered_counts.csv', sep='/')
-print(paste("writing filtered counts csv to: ", filtrc_out_file))
-write.csv(filtered_counts, filtrc_out_file, row.names=F, quote=F)
+write_results_to_csv = function(results, outpath) {
+  # Write out module outputs ----
+  qc_table = results$qc_table
+  qc_out_file = paste(outpath, 'QC_table.csv', sep='/')
+  print(paste("writing QC_table to: ", qc_out_file))
+  write.csv(qc_table, qc_out_file, row.names=F, quote=F)
+  
+  unmapped_reads= results$unmapped_reads
+  unmapped_out = paste(outpath, 'unmapped_reads.csv', sep='/')
+  print(paste("writing unmapped reads to: ", unmapped_out))
+  write.csv(unmapped_reads, unmapped_out, row.names=F)
+  
+  annotated_counts = results$annotated_counts
+  annot_out_file = paste(outpath, 'annotated_counts.csv', sep='/')
+  print(paste("writing annotated counts to: ", annot_out_file))
+  write.csv(annotated_counts, annot_out_file, row.names=F)
+  
+  filtered_counts = results$filtered_counts
+  filtrc_out_file = paste(outpath, 'filtered_counts.csv', sep='/')
+  print(paste("writing filtered counts csv to: ", filtrc_out_file))
+  write.csv(filtered_counts, filtrc_out_file, row.names=F, quote=F)
+  
+  if (results$filtered_counts_original) {
+    # keep the full filtered counts with the data that needs to be removed
+    write.csv(results$filtered_counts_original, paste(outpath, 'filtered_counts_original.csv', sep='/'), row.names=F, quote=F)
+  }
+}
 
+main = function() {
+  # Arguement parser ----
+  parser <- ArgumentParser()
+  # specify desired options
+  parser$add_argument("-v", "--verbose", action="store_true", default=TRUE,
+                      help="Print extra output [default]")
+  parser$add_argument("-q", "--quietly", action="store_false",
+                      dest="verbose", help="Print little output")
+  parser$add_argument("--wkdir", default=getwd(), help="Working directory")
+  parser$add_argument("-c", "--raw_counts", default="raw_counts.csv", help = "path to file containing raw counts")
+  parser$add_argument("-o", "--out", default="", help = "Output path. Default is working directory")
+  parser$add_argument("-s", "--sample_meta", default="sample_meta.csv", help = "Sample metadata")
+  parser$add_argument("--cell_line_meta", default="cell_line_meta.csv", help = "Cell Line metadata")
+  parser$add_argument("--cell_set_meta", default="cell_set_meta.csv", help = "Cell set metadata")
+  parser$add_argument("--assay_pool_meta", default="assay_pool_meta.txt", help = "Assay pool metadata")
+  parser$add_argument("--CB_meta", default="CB_meta.csv", help = "Control Barcode metadata")
+  parser$add_argument("--sequencing_index_cols", default= "index_1,index_2", 
+                      help = "Sequencing columns in the sample meta")
+  parser$add_argument("--count_threshold", default= 40, help = "Low counts threshold")
+  parser$add_argument("--reverse_index2", type="logical",
+                      help = "Reverse complement of index 2 for NovaSeq and NextSeq")
+  parser$add_argument("--rm_data", type="logical", help = "Remove bad experimental data")
+  parser$add_argument("--pool_id", type="logical", help = "Pull pool IDs from CellDB.")
+  parser$add_argument("--control_type", default="negcon", 
+                      help = "negative control wells in trt_type column in sample metadata")
+  
+  # get command line options, if help option encountered print help and exit
+  args <- parser$parse_args()
+  
+  # set output to working directory if none is specified
+  if (args$out == ""){
+    args$out = args$wkdir
+  }
+  #print_args(args)
+  
+  # Read in files and set up parameters ----
+  cell_set_meta= data.table::fread(args$cell_set_meta, header= T, sep= ',', data.table= F)
+  cell_line_meta= data.table::fread(args$cell_line_meta, header= T, sep= ',', data.table= F)
+  CB_meta= data.table::fread(args$CB_meta, header= T, sep= ',', data.table= F)
+  sample_meta= data.table::fread(args$sample_meta, header= T, sep= ',', data.table= F)
+  raw_counts= data.table::fread(args$raw_counts, header= T, sep= ',', data.table= F)
+  
+  validate_cell_line_meta(cell_line_meta)
+  validate_seq_index_cols(args$sequencing_index_cols, sample_meta)
+  cell_line_meta = remove_duplicate_luas(cell_line_meta)
+  
+  # Run filter_raw_reads -----
+  print("creating filtered count file")
+  filtered_counts = filter_raw_reads(raw_counts,
+                                     sample_meta,
+                                     cell_line_meta,
+                                     cell_set_meta,
+                                     CB_meta,
+                                     sequencing_index_cols= sequencing_index_cols,
+                                     count_threshold= as.numeric(args$count_threshold),
+                                     reverse_index2= args$reverse_index2)
+  
+  validate_required_cols(filtered_counts$filtered_counts, filtered_counts_required_cols)
+  unique_cell_sets <- unique(sample_meta$cell_set[sample_meta$cell_set != ""])
+  
+  if (args$pool_id) {
+    assay_pool_meta = read.delim(args$assay_pool_meta)
+    validate_required_cols(assay_pool_meta, assay_pool_required_cols)
+    filtered_counts$filtered_counts = merge_pool_info(filtered_counts$filtered_counts, assay_pool_meta, unique_cell_sets)
+    filtered_counts$annotated_counts = merge_pool_info(filtered_counts$annotated_counts, assay_pool_meta, unique_cell_sets)
+  }
+  
+  
+  # Validation: Basic file size check ----
+  if(sum(filtered_counts$filtered_counts$n) == 0) {
+    stop('All entries in filtered counts are missing!')
+  }
+  
+  cl_entries= filtered_counts$filtered_counts %>% dplyr::filter(!is.na(CCLE_name))
+  if(sum(cl_entries$n) == 0) {
+    stop('All cell line counts are zero!')
+  }
+  
+  print(paste("rm_data:", args$rm_data))
+  if(args$rm_data == TRUE){
+    filtered_counts$filtered_counts_original <- filtered_counts$filtered_counts
+    filtered_counts$filtered_counts = remove_data_from_filter_counts(filtered_counts$filtered_counts, args$out)
+    
+    # re-point to what filtered_counts should be
+    filtered_counts <- filt_rm
+    rows_removed = nrow(filtered_counts_original) - nrow(filtered_counts)
+    paste("Number of rows removed: ", rows_removed)
+  }
+  
+  write_results_to_csv(filtered_counts, args$out)
+}
+
+# main()
--- tests/testthat/test_filter_counts.R
+++ tests/testthat/test_filter_counts.R
@@ -1,21 +1,49 @@
 #context("Test filter counts")
+source("filter_counts.R")
 library(magrittr)
-library(prismSeqR)
 
 test_that("Test filter counts function", {
-  test_dir = test_path('assets')
-  
-  rc = read.csv(paste(test_dir, 'test_raw_counts.csv',sep='/'))
-  
-  sm = read.csv(paste(test_dir, 'test_meta.csv',sep='/'))
-  
-  cell_meta = read.csv(paste(test_dir, 'test_cell_line_meta.csv',sep='/'))
-  cell_set_meta = read.csv(paste(test_dir, 'test_cell_set_meta.csv',sep='/'))
-  CB_meta = read.csv(paste(test_dir, 'CB_meta.csv',sep='/'))
-  
-  filtered_counts = filter_raw_reads(rc, sm, cell_meta, cell_set_meta, CB_meta, id_cols = c('treatment', 'dose','dose_unit','day') )
-  
-  expect_equal(nrow(filtered_counts$filtered_counts), 69)
-  expect_equal(length(filtered_counts$filtered_counts), 17)
+  expect_error(validate_cell_line_meta("Index1"), "Cell line meta validation not yet implemented")
 }
 )
+test_that("Test LUA duplicates in cell line meta with duplicates", {
+  cell_line_meta_with_duplicates <- data.frame(LUA = c("LUA-1000", "LUA-1001", "LUA-1002", "LUA-1001"))
+  expected_result <- data.frame(LUA = c("LUA-1000", "LUA-1001", "LUA-1002"))
+  luas_removed = remove_duplicate_luas(cell_line_meta_with_duplicates)
+  expect_equal(luas_removed$LUA %>% sort(), expected_result$LUA %>% sort()) 
+  expect_equal(length(luas_removed$LUA), length(expected_result$LUA)) 
+  })
+
+# Classes are different between LUAs removed and expected result - LUA column needs to be subsetted for comparison
+test_that("Test LUA duplicates in cell line meta without duplicates", {
+  cell_line_meta <- data.frame(LUA = c("LUA-1000", "LUA-1001", "LUA-1002"))
+  expected_result <- data.frame(LUA = c("LUA-1000", "LUA-1001", "LUA-1002"))
+  luas_removed = remove_duplicate_luas(cell_line_meta)
+  expect_equal(luas_removed$LUA, expected_result$LUA) 
+})
+
+
+test_that("Test merge assay pool meta function with pool_id", {
+  test_assay_pool_meta <- data.frame(depmap_id = "ACH-000956", ccle_name = "22RV1_PROSTATE",
+                                     davepool_id = "EXT.PR500.CS01.2.B", pool_id = "P111")
+  
+  test_filtered_counts <- data.frame(CCLE_name = "22RV1_PROSTATE", cell_set = "EXT.PR500.CS01.2.B", 
+                                     DepMap_ID = "ACH-000956", random_column = "X")
+  
+  davepool_ids = c("EXT.PR500.CS01.2.B")
+  expected_result = test_filtered_counts
+  expected_result$pool_id = "P111"
+  expect_equal(merge_pool_info(test_filtered_counts, test_assay_pool_meta, davepool_ids), expected_result)
+})
+
+test_that("Test merge assay pool meta function with no pool_id", {
+  test_assay_pool_meta <- data.frame(depmap_id = "ACH-000956", ccle_name = "22RV1_PROSTATE",
+                                     davepool_id = "EXT.PR500.CS01.2.B")
+  
+  test_filtered_counts <- data.frame(CCLE_name = "22RV1_PROSTATE", cell_set = "EXT.PR500.CS01.2.B", 
+                                     DepMap_ID = "ACH-000956", random_column = "X")
+  
+  davepool_ids = c("EXT.PR500.CS01.2.B")
+  expect_error(merge_pool_info(test_filtered_counts, test_assay_pool_meta, davepool_ids), "Column `pool_id` doesn't exist.")
+})
+
