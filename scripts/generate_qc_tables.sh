baa86cdbc402934f39f9a8559cc8f4457bd895b8
jdavis3141 <jdavis@broadinstitute.org>
2024-11-21 14:12:39 -0500

    better abs path check, add filtered_counts

--- scripts/generate_qc_tables.sh
+++ scripts/generate_qc_tables.sh
@@ -7,38 +7,31 @@ then
 	echo BUILD_DIR not specified
     exit -1
 fi
-
+echo Build dir is: $BUILD_DIR
 
 #Enforces abs paths
-if [[ "$NORMALIZED_COUNTS" = /* ]]
-then
-	NORMALIZED_COUNTS=$(ls $NORMALIZED_COUNTS)
-else
-	NORMALIZED_COUNTS=$BUILD_DIR/$NORMALIZED_COUNTS
-fi
 
-echo Build dir is: $BUILD_DIR
-echo NORMALIZED_COUNTS is: $NORMALIZED_COUNTS
+enforce_abs_path() {
+  local var_name=$1
+  local value=${!var_name}
 
-if [[ "$ANNOTATED_COUNTS" = /* ]]
-then
-  ANNOTATED_COUNTS=$(ls $ANNOTATED_COUNTS)
-else
-  ANNOTATED_COUNTS=$BUILD_DIR/$ANNOTATED_COUNTS
-fi
-echo ANNOTATED_COUNTS is: $ANNOTATED_COUNTS
+  if [[ "$value" = /* ]]; then
+    eval "$var_name=$(ls "$value")"
+  else
+    eval "$var_name=$BUILD_DIR/$value"
+  fi
+  echo "$var_name is: ${!var_name}"
+}
 
-if [[ "$CELL_SET_AND_POOL_META" = /* ]]
-then
-  CELL_SET_AND_POOL_META=$(ls $CELL_SET_AND_POOL_META)
-else
-  CELL_SET_AND_POOL_META=$BUILD_DIR/$CELL_SET_AND_POOL_META
-fi
-echo CELL_SET_AND_POOL_META is: $CELL_SET_AND_POOL_META
+enforce_abs_path NORMALIZED_COUNTS
+enforce_abs_path ANNOTATED_COUNTS
+enforce_abs_path CELL_SET_AND_POOL_META
+enforce_abs_path FILTERED_COUNTS
 
 args=(
 --normalized_counts "$NORMALIZED_COUNTS"
 --annotated_counts "$ANNOTATED_COUNTS"
+--filtered_counts "$FILTERED_COUNTS"
 --negcon_type "$CTL_TYPES"
 --poscon_type "$POSCON_TYPE"
 --cell_set_and_pool_meta "$CELL_SET_AND_POOL_META"
