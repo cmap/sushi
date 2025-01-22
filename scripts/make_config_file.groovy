218951914781991527faaaa44b241103e3de5beb
jdavis3141 <jdavis@broadinstitute.org>
2025-01-22 11:01:12 -0500

    typo in drc path

--- scripts/make_config_file.groovy
+++ scripts/make_config_file.groovy
@@ -95,7 +95,7 @@ pipeline {
         // Biomarker
         string(name: 'BIOMARKER_FILE', defaultValue: '/data/biomarker/current/depmap_datasets_public.h5', description: 'Biomarker reference file.')
         string(name: 'AUC_COLUMN', defaultValue: 'log2_auc', description: 'Name of the column containing AUC values used in biomarker analysis. This defaults to \"log2_auc\".')
-        string(name: 'DRC_PATH', defaultValue: 'drc/dose_reponse.csv', description: 'File in BUILD_DIR containing dose response curve data. This file is created by DRC.')
+        string(name: 'DRC_PATH', defaultValue: 'drc/dose_response.csv', description: 'File in BUILD_DIR containing dose response curve data. This file is created by DRC.')
     }
 
     environment {
