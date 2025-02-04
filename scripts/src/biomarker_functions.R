fa0e04226c0f7a6cbd2e94c0597b658b10feaa57
jdavis3141 <jdavis@broadinstitute.org>
2025-02-04 12:14:01 -0500

    added some debugging to biomarker

--- scripts/src/biomarker_functions.R
+++ scripts/src/biomarker_functions.R
@@ -517,6 +517,8 @@ create_multivariate_biomarker_table <- function(in_path, out_path = NULL,
   necessary_columns <- unique(c(response_column, treatment_columns, "depmap_id"))
   print(paste0("DEBUG: necessary_columns: ", necessary_columns))
   print(paste0("DEBUG: input columns: ", colnames(input_file)))
+  print(paste0("DEBUG: treatment_columns: ", treatment_columns))
+  print(paste0("DEBUG: response_column: ", response_column))
   if(any(!necessary_columns %in% colnames(input_file))){
     stop(paste0(paste0(setdiff(necessary_columns, colnames(input_file)), collapse = ", "),
                 " are missing from multivariate biomarker input file!"))
@@ -600,8 +602,10 @@ create_univariate_biomarker_table <- function(in_path, out_path = NULL,
 
   # check for missing critical columns
   necessary_columns <- unique(c(response_column, treatment_columns, "depmap_id"))
+  print(paste0("DEBUG: treatment_columns: ", treatment_columns))
   print(paste0("DEBUG: necessary_columns: ", necessary_columns))
   print(paste0("DEBUG: input columns: ", colnames(input_file)))
+  print(paste0("DEBUG: response_column: ", response_column))
   if(any(!necessary_columns %in% colnames(input_file))){
     stop(paste0(paste0(setdiff(necessary_columns, colnames(input_file)), collapse = ", "),
                 " are missing in univariate biomarker input file!"))
