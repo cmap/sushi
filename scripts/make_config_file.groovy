fe100b0aa185778854a2c62b98f14ef3e8002e86
jdavis3141 <jdavis@broadinstitute.org>
2025-02-24 12:48:32 -0500

    finalize default merge_patterns

--- scripts/make_config_file.groovy
+++ scripts/make_config_file.groovy
@@ -149,7 +149,7 @@ pipeline {
         string(name: 'COLLAPSED_LFC', defaultValue: 'collapsed_l2fc.csv', description: 'Filename in BUILD_DIR containing replicate collapsed l2fc values. This file is created by COLLAPSED_LFC.')
         // Other
         string(name: 'API_URL', defaultValue: 'https://api.clue.io/api/', description: 'API URL')
-        string(name: 'MERGE_PATTERNS', defaultValue: '^normalized_counts*,^collapsed_l2fc*,^l2fc*,^log2_auc_multivariate_biomarkers*,^log2_auc_univariate_biomarkers*,^median_l2fc_multivariate_biomarkers*,^median_l2fc_univariate_biomarkers*,^DRC_TABLE*', description: 'Patterns to search for when merging files by project. May be changed based on modules run.')
+        string(name: 'MERGE_PATTERNS', defaultValue: 'normalized_counts*,collapsed_l2fc*,l2fc*,log2_auc_multivariate_biomarkers*,log2_auc_univariate_biomarkers*,median_l2fc_multivariate_biomarkers*,median_l2fc_univariate_biomarkers*,DRC_TABLE*', description: 'Patterns to search for when merging files by project. May be changed based on modules run.')
 
         // Biomarker
         string(name: 'BIOMARKER_FILE', defaultValue: '/data/biomarker/current/depmap_datasets_public.h5', description: 'Biomarker reference file.')
