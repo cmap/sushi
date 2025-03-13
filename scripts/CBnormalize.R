c6f05c0816d1d2d8238a927d89862a0cc2fc2489
jdavis3141 <jdavis@broadinstitute.org>
2025-03-13 10:14:54 -0400

    remove unused compute_read_stats in normalize

--- scripts/CBnormalize.R
+++ scripts/CBnormalize.R
@@ -35,13 +35,6 @@ annotated_counts = fread(args$annotated_counts, header= TRUE, sep= ',')
 unknown_counts = fread(args$unknown_barcode_counts, header= TRUE, sep= ',')
 cell_set_meta = fread(args$cell_set_meta, header= TRUE, sep= ',')
 
-# Generate first set of QC flag results
-flagged_results = compute_read_stats(filtered_counts= filtered_counts, annotated_counts= annotated_counts,
-                                     unknown_counts= unknown_counts, cell_set_meta= cell_set_meta)
-
-# Get dataframe to run through normalization ----
-norm_df = flagged_results$result
-
 # Run normalize ----
 print("Creating normalized count file ...")
 normalized_counts = normalize(X= norm_df, id_cols= input_id_cols,
