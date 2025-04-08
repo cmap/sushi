f764bcc5dcc012d57aa2fe1af58ae19f417ce04d
YuhJong Liu <yuhjong24@gmail.com>
2025-04-08 15:02:18 -0400

    Corrected column names

--- scripts/src/synergy.R
+++ scripts/src/synergy.R
@@ -5,30 +5,38 @@
 restructure_l2fc = function(cps_l2fc, sig_cols, cell_line_cols,
                             singles_type = "trt_cp",
                             combos_type = "trt_combo",
-                            l2fc_col = 'median_l2fc') {
+                            l2fc_col = "median_l2fc",
+                            names_prefix = c("pert1", "pert2", "combo"),
+                            names_sep = "_") {
+  # New column names
+  new_names = paste(names_prefix, l2fc_col, sep = names_sep)
   
   # Slice data frame into single agents and combos
   singles_df = cps_l2fc %>% dplyr::filter(pert_type == singles_type)
   combos_df = cps_l2fc %>% dplyr::filter(pert_type == combos_type) %>%
-    dplyr::rename_with(~ paste0("combo_", .x), contains(l2fc_col))
+    dplyr::rename(setNames(l2fc_col, new_names[3]))
+  # Change the old l2fc_col name into combo + old l2fc_col
   
-  # Add single agents to pert1 of the combos
-  join_cols = sig_cols[sig_cols != "pert_type" & !grepl('pert2_', sig_cols)]
+  # Add single agents to pert1 of the combos dataframe
+  join_cols = sig_cols[sig_cols != "pert_type" & !grepl(names_prefix[2], sig_cols)]
   
-  restructured_l2fc = combos_df %>% 
-    dplyr::inner_join(singles_df %>% dplyr::rename_with(~ paste0("pert1_", .x), contains(l2fc_col)),
-                      by = c(cell_line_cols, join_cols), suffix = c('', '.y')) %>%
-    dplyr::relocate(all_of(c(cell_line_cols, sig_cols, 'LFC'))) %>% dplyr::select(!contains('.y'))
+  restructured_l2fc = combos_df %>%
+    dplyr::left_join(singles_df %>% dplyr::rename(setNames(l2fc_col, new_names[1])),
+                     by = c(cell_line_cols, join_cols), 
+                     suffix = c("", ".y")) %>%
+    dplyr::relocate(any_of(c(cell_line_cols, sig_cols, new_names))) %>%
+    dplyr::select(!contains(".y"))
   
-  # Add single agents to pert2 of the combos
-  join_cols = sig_cols[!grepl('pert', sig_cols)]
-  cross_join_cols = setNames(sig_cols[grepl('pert1_', sig_cols)], 
-                             sig_cols[grepl('pert2_', sig_cols)])
+  # Add single agents to pert2 of the combos dataframe
+  join_cols = sig_cols[!grepl("pert", sig_cols)]
+  cross_join_cols = setNames(sig_cols[grepl(names_prefix[1], sig_cols)],
+                             sig_cols[grepl(names_prefix[2], sig_cols)])
   
   restructured_l2fc = restructured_l2fc %>% 
-    dplyr::inner_join(singles_df %>% dplyr::rename_with(~ paste0("pert2_", .x), contains(l2fc_col)),
-                      by = c(cell_line_cols, join_cols, cross_join_cols), suffix = c('', '.y')) %>%
-    dplyr::relocate(all_of(c(cell_line_cols, sig_cols, 'LFC'))) %>% dplyr::select(!contains('.y'))
+    dplyr::left_join(singles_df %>% dplyr::rename(setNames(l2fc_col, new_names[2])),
+                     by = c(cell_line_cols, join_cols, cross_join_cols), suffix = c("", ".y")) %>%
+    dplyr::relocate(any_of(c(cell_line_cols, sig_cols, new_names))) %>% 
+    dplyr::select(!contains(".y"))
   
   return(restructured_l2fc)
 }
@@ -82,21 +90,31 @@ restructure2_l2fc = function(cps_l2fc, sig_cols, cell_line_cols,
 # uses the first restructuring function
 # l2fc_root is set to l2fc_col
 calculate_synergy = function(restructured_l2fc,
-                           l2fc_root = 'median_l2fc',
-                           cap_for_viability = 1) {
+                             l2fc_root = "median_l2fc",
+                             viability_cap = 1,
+                             names_prefix = c("pert1", "pert2", "combo"),
+                             names_sep = "_") {
+  # Create vector of names used to create new columns
+  l2fc_names = paste(names_prefix, l2fc_root, sep = names_sep)
+  viab_names = paste(names_prefix, "viab", sep = names_sep)
+
   # From l2fc calculate FCs
-  synergy_scores = restructured_l2fc %>% 
-    dplyr::mutate(pert1_capped_viab = pmin(2^get(paste0('pert1_', l2fc_root)), cap_for_viability),
-                  pert2_capped_viab = pmin(2^get(paste0('pert2_', l2fc_root)), cap_for_viability),
-                  combo_capped_viab = pmin(2^get(paste0('combo_', l2fc_root)), cap_for_viability),
-                  bliss = pert1_capped_viab * pert2_capped_viab) %>%
-    dplyr::rowwise() %>% 
-    dplyr::mutate(hsa = min(pert1_capped_viab, pert2_capped_viab)) %>% 
+  restructured_l2fc[[viab_names[1]]] = pmin(2^restructured_l2fc[[l2fc_names[1]]], viability_cap)
+  restructured_l2fc[[viab_names[2]]] = pmin(2^restructured_l2fc[[l2fc_names[2]]], viability_cap)
+  restructured_l2fc[[viab_names[3]]] = pmin(2^restructured_l2fc[[l2fc_names[3]]], viability_cap)
+  
+  # Use viab to calculate bliss, hsa, and synergy
+  synergy_scores = restructured_l2fc %>%
+    dplyr::mutate(bliss = .data[[viab_names[1]]] * .data[[viab_names[2]]]) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(hsa = min(.data[[viab_names[1]]], .data[[viab_names[2]]],
+                            na.rm = FALSE)) %>%
     dplyr::ungroup() %>%
-    dplyr::mutate(synergy = 
-                    dplyr::case_when(combo_capped_viab > hsa ~ combo_capped_viab - hsa,
-                                     combo_capped_viab < bliss ~ bliss - combo_capped_viab,
-                                     .default = 0))
+    dplyr::mutate(synergy =
+                    dplyr::case_when(.data[[viab_names[3]]] > hsa ~ .data[[viab_names[3]]] - hsa,
+                                     bliss <= .data[[viab_names[1]]] & .data[[viab_names[1]]] <= hsa ~ 0,
+                                     .data[[viab_names[3]]] < bliss ~ bliss - .data[[viab_names[3]]],
+                                     .default = NA))
   
   return(synergy_scores)
 }
@@ -104,29 +122,60 @@ calculate_synergy = function(restructured_l2fc,
 # Sample DMSO l2fc values
 sample_dmso_l2fc = function(dmso_l2fc, l2fc_col = 'l2fc',
                             grouping_cols,
-                            n_samples) {
-  LFC_sample <- list()
-  for (i in 1:n_samples){
-    LFC_sample[[i]] <-  dmso_l2fc %>%
-      group_by(pick(all_of(grouping_cols))) %>%
-      slice_sample(n = 3) %>%
-      summarise(pert1_LFC = median(.[[l2fc_col]]), .groups = 'drop')
-    if(i %% 50 == 0) { print(i) }
-  }
-  LFC_sample <- LFC_sample %>%
-    dplyr::bind_rows()
-  
-  LFC_sampled = LFC_sample %>% 
+                            n_samples,
+                            names_prefix = c("pert1", "pert2", "combo"),
+                            names_sep = "_",
+                            seed = 2) {
+  # Check that there are enough entries to sample to n
+  min_num_reps = dmso_l2fc %>%
     dplyr::group_by(pick(all_of(grouping_cols))) %>%
-    dplyr::mutate(pert2_LFC = sample(pert1_LFC, n_samples, replace = TRUE),
-                  combo_LFC = sample(pert1_LFC, n_samples, replace = TRUE)) %>%
+    dplyr::summarize(count= dplyr::n(), .groups = "drop") %>%
+    dplyr::pull(count) %>% base::min()
+  # Calculate combination with the minimum
+  num_pick_combinations = base::choose(min_num_reps, 3)
+  print(paste0(min_num_reps, " choose ", 3, " = ", num_pick_combinations))
+  
+  # Stop if the number to sample to is too low
+  if(num_pick_combinations < n_samples) {
+    stop("ERROR: Cannot sample up to n.")
+  }
+    
+  # Set seed
+  base::set.seed(seed)
+  # Create vector of names used to create new columns
+  l2fc_names = paste(names_prefix, l2fc_col, sep = names_sep)
+  # Create an empty list to store results
+  sample_output = list()
+  
+  # Loop to sample each cell line on a plate
+  for(i in 1:n_samples) {
+    sample_output[[i]] = dmso_l2fc %>%
+      dplyr::group_by(pick(all_of(grouping_cols))) %>%
+      dplyr::slice_sample(n = 3) %>%
+      dplyr::summarise(!!l2fc_names[1] := median(.data[[l2fc_col]]), .groups = 'drop')
+    
+    if(i %% 100 == 0) { print(i) }
+  }
+  
+  # Combine sampled outputs and sample other two l2fc columns
+  sample_l2fc = dplyr::bind_rows(sample_output) %>%
+    dplyr::group_by(pick(all_of(grouping_cols))) %>%
+    dplyr::mutate(!!l2fc_names[2] := sample(.data[[l2fc_names[1]]], n_samples, replace = TRUE),
+                  !!l2fc_names[3] := sample(.data[[l2fc_names[1]]], n_samples, replace = TRUE)) %>%
     dplyr::ungroup()
-  return(LFC_sampled)
+  
+  return(sample_l2fc)
 }
 
-# 
+# Get cdf to change into pvalue
 get_cdf_value = function(group_name, synergy, h5_file) {
   ecdf_obj = stats::ecdf(h5read(h5_file, group_name))
   out_value = ecdf_obj(synergy)
   return(out_value)
+}
+
+# smooth pvalue
+smooth_pvalue <- function(pval_naive, n_samples ){
+  ## use Laplace smoothing on pvalue to not output a 0 pvalue.
+  return ( (pval_naive * n_samples+1)/(n_samples+2) )
 }
