3d2dc111a7b8edb7deb38765fea038ef8c9ac9f8
jdavis3141 <jdavis@broadinstitute.org>
2025-04-08 12:11:39 -0400

    support luas and depmap_ids

--- scripts/src/cellDB_metadata.R
+++ scripts/src/cellDB_metadata.R
@@ -42,24 +42,37 @@ get_cell_api_info = function(api_url, api_key, filter = NULL) {
   }
 }
 
+
 get_LUAs_from_sets <- function(cell_set_name) {
   
   cell_set_definition_api_url <- "https://api.clue.io/api/e_cell_set_definition_files"
   # Construct the filter object as a JSON string
   filter <- list(where = list(davepool_id = cell_set_name),
-                 fields = c("davepool_id","depmap_id")
+                 fields = c("davepool_id","depmap_id","barcode_id")
   )
   cell_set_DepMap_df <- get_cell_api_info(cell_set_definition_api_url, api_key, filter)
   depmap_ids <- list(cell_set_DepMap_df$depmap_id)
-  return(depmap_ids)
+  luas <- list(cell_set_DepMap_df$barcode_id)
+  res <- data.table(
+    cell_set = cell_set_name,
+    depmap_id = unlist(depmap_ids),
+    lua = unlist(luas)
+  )
+  return(res)
 }
 
 get_LUAs_from_pools <- function(cell_pool_name) {
   v_assay_pool_api_url <- "https://api.clue.io/api/v_e_assay_pools"
-  filter <- list(where = list(assay_pool = cell_pool_name), fields = c("assay_pool","barcode_id"))
+  filter <- list(where = list(assay_pool = cell_pool_name), fields = c("assay_pool","barcode_id", "depmap_id"))
   cell_pool_DepMap_df <- get_cell_api_info(v_assay_pool_api_url, api_key, filter)
   luas <- list(cell_pool_DepMap_df$barcode_id)
-  return(luas)
+  depmap_ids <- list(cell_pool_DepMap_df$depmap_id)
+    res <- data.table(
+        cell_set = cell_pool_name,
+        depmap_id = unlist(depmap_ids),
+        lua = unlist(luas)
+    )
+  return(res)
 }
 
 create_cell_set_meta = function(sample_meta, cell_sets_df, cell_pools_df, cell_line_meta) {
@@ -89,22 +102,28 @@ create_cell_set_meta = function(sample_meta, cell_sets_df, cell_pools_df, cell_l
 
         # Collecting and storing set DepMap ID members   
         cs_members = get_LUAs_from_sets(cs[j])
-        all_DepMapIDs = append(all_DepMapIDs, cs_members)
-        all_luas = append(all_luas, cs_members)
-        
+        all_DepMapIDs = append(all_DepMapIDs, list(cs_members$depmap_id))
+        all_luas = append(all_luas, list(cs_members$lua))
+
       } else if (cs[j] %in% cell_pools_df$name){
         print(paste(cs[j], "is a cell pool")) 
         known_cell_sets = append(known_cell_sets, cs[j])
         
         # Collecting pool lua members
         pool_members = get_LUAs_from_pools(cs[j])
-        all_luas = append(all_luas, pool_members)
+        all_luas = append(all_luas, pool_members$lua)
+        all_DepMapIDs = append(all_DepMapIDs, pool_members$depmap_id)
+
         # all_DepMapIDs = append(all_DepMapIDs, pool_members)
         print(paste(cs[j], "has a lua length of:", length(pool_members[[1]])))
         
       } else if (cs[j] %in% cell_line_meta$lua){
-        print(paste(cs[j], "is a cell line")) 
-        all_luas = append(all_luas, cs[j])
+        print(paste(cs[j], "is a cell line"))
+        member = cell_line_meta %>%
+          filter(lua == cs[j]) %>%
+          select(depmap_id, lua)
+        all_DepMapIDs = append(all_DepMapIDs, member$depmap_id)
+        all_luas = append(all_luas, member$lua)
         known_cell_sets = c(known_cell_sets, cs[j])
         
       } else {
@@ -120,10 +139,11 @@ create_cell_set_meta = function(sample_meta, cell_sets_df, cell_pools_df, cell_l
         known_cell_sets <- paste(known_cell_sets, collapse = ";")
         joined_luas <- sapply(all_luas, function(row) paste(row, collapse = ";"))
         all_luas <- paste(joined_luas, collapse = ";")
-        #joined_DepMapIDs <- sapply(all_DepMapIDs, function(row) paste(row, collapse = ";"))
-        #all_DepMapIDs <- paste(joined_DepMapIDs, collapse = ";")
+        joined_DepMapIDs <- sapply(all_DepMapIDs, function(row) paste(row, collapse = ";"))
+        all_DepMapIDs <- paste(joined_DepMapIDs, collapse = ";")
         cell_set_meta$cell_set[insert_index] <- known_cell_sets
-        cell_set_meta$members[insert_index] <- all_luas
+        cell_set_meta$lua_members[insert_index] <- all_luas
+        cell_set_meta$depmap_id_members[insert_index] <- all_DepMapIDs
       }
     } else {
       failed_sets <- c(failed_sets, chr_unique_cell_sets)
