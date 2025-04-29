58dbc77e99fc205800188b327de9740107a1db2f
manilkocak <35981624+manilkocak@users.noreply.github.com>
2025-04-29 11:09:29 -0400

    typo corrected to keep the CRISPR features

--- scripts/src/biomarker_functions.R
+++ scripts/src/biomarker_functions.R
@@ -447,7 +447,9 @@ multivariate_biomarker_table <- function(Y, W = NULL, file, k = 10) {
     rf_RNA <- random_forest(cbind(X$X.DNA[cl_, ], X$X.RNA[cl_, ]), y[cl_], folds = rf_DNA$folds, k = k)
     
     cl_ = intersect(cl_, rownames(X$X.CRISPR))
-    rf_CRISPR <- random_forest(cbind(X$X.DNA[cl_, ], X$X.RNA[cl_, ], X$CRISPR[cl_, ]), y[cl_], k = k)
+    
+    
+    rf_CRISPR <- random_forest(cbind(X$X.DNA[cl_, ], X$X.RNA[cl_, ], X$X.CRISPR[cl_, ]), y[cl_], k = k)
 
     rf.DNA[[ix]] <- rf_DNA$model_table %>%
       dplyr::mutate(y = colnames(Y)[ix],
