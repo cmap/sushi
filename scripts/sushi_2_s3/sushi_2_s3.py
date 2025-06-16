2520f82dead85e2c7b1cfde3e897d1f40a641c06
jdavis3141 <jdavis@broadinstitute.org>
2025-06-16 15:00:42 -0400

    added logging statement for day filtering

--- scripts/sushi_2_s3/sushi_2_s3.py
+++ scripts/sushi_2_s3/sushi_2_s3.py
@@ -226,16 +226,14 @@ def main(args):
     if days:
         days = [int(day.strip()) for day in days.split(",")]
         logger.info(f"Filtering dataframes to only include days: {days}")
+        logger.info("Reading SushiBuild object...")
+        build = SushiBuild(build_path)
+        for name, df in build:
+            logger.info(name, df.schema, df.shape)
+        logger.info(f"Loaded build: {build}")
         build.update_tables(lambda df: df.filter(pl.col("day").is_in(days)))
     else:
         logging.info("No days specified, not filtering dataframes.")
-
-    logger.info("Reading SushiBuild object...")
-    build = SushiBuild(build_path)
-    for name, df in build:
-        print(name, df.schema, df.shape)
-    logger.info(f"Loaded build: {build}")
-
 
     # Sync the build directory to S3
     logger.info(f"Syncing {build_path} to {s3_prefix}...")
