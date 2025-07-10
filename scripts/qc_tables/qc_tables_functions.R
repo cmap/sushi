26a6890023301d95ab6ea30785c3174dbeee2471
jdavis3141 <jdavis@broadinstitute.org>
2025-07-10 11:02:43 -0400

    Ensure id_cols table is de-duplicated

--- scripts/qc_tables/qc_tables_functions.R
+++ scripts/qc_tables/qc_tables_functions.R
@@ -228,7 +228,7 @@ generate_id_cols_table <- function(annotated_counts, normalized_counts, unknown_
     dplyr::left_join(cb_metrics, by = c(id_cols_list, "pert_plate")) %>%
     dplyr::left_join(normalized_counts %>% dplyr::select(c(id_cols_list, "cell_set")),
       by = id_cols_list
-    )
+    ) %>% dplyr::distinct()
 
   return(id_cols_table)
 }
