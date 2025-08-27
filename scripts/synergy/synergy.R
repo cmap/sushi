578f448f9493e6efb4136c443ab898a225612c2c
jdavis3141 <jdavis@broadinstitute.org>
2025-08-27 14:02:32 -0400

    hotfix: add pert_vehicle to ignored cols for synergy checks

--- scripts/synergy/synergy.R
+++ scripts/synergy/synergy.R
@@ -89,7 +89,7 @@ names_prefix = c("pert", "pert2", "combo")
 new_names = paste(names_prefix, args$l2fc_col, sep = "_")
 
 # Pull out columns that describe each perturbation
-ignore_cols = c("pert_type", "pert_plate") # common columns with "pert" to be ignored
+ignore_cols = c("pert_type", "pert_plate", "pert_vehicle") # common columns with "pert" to be ignored
 pert1_cols = args$sig_cols[grepl(paste0(names_prefix[1], "_"), args$sig_cols) & !args$sig_cols %in% ignore_cols]
 pert2_cols = args$sig_cols[grepl(paste0(names_prefix[2], "_"), args$sig_cols) & !args$sig_cols %in% ignore_cols]
 
