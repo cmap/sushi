8d19da6429ef3ac8f9098111bbfe0230df4deb40
YuhJong Liu <yuhjong24@gmail.com>
2025-10-01 13:37:32 -0400

    Added na filter

--- scripts/growth_correction/growth_correction_functions.R
+++ scripts/growth_correction/growth_correction_functions.R
@@ -2,7 +2,7 @@
 # Function expects annotation column to be "growth_pattern"
 apply_growth_correction = function(df, raw_l2fc_col = "l2fc", growth_pattern_col = "growth_pattern") {
   centered_df = df |>
-    dplyr::filter(is.finite(.data[[raw_l2fc_col]])) |>
+    dplyr::filter(is.finite(.data[[raw_l2fc_col]]), !is.na(.data[[growth_pattern_col]])) |>
     dplyr::mutate(growth_pattern = as.factor(.data[[growth_pattern_col]]),
                   centered_l2fc = .data[[raw_l2fc_col]] - mean(.data[[raw_l2fc_col]]))
 
